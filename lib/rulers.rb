a5cf5f77c171a7b3af880f7bb348209cd967c3f0
Vasyl Mytskaniuk <vasul2537125@gmail.com>
2020-10-08 00:53:48 +0300

    add application class

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,6 +1,11 @@
 require "rulers/version"
 
 module Rulers
-  class Error < StandardError; end
-  # Your code goes here...
+  # class Error < StandardError; end
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'},
+        ["Hello from Ruby On Rulers!"]]
+    end
+  end
 end
